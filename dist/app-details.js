6e75812c5e60ad94d75588dc7657643488454bb4
Hanesj <hannes.sjostrom96@gmail.com>
2025-02-07 04:44:10 +0100

    Handle form and booking

--- dist/app-details.js
+++ dist/app-details.js
@@ -27,20 +27,75 @@ const loadCourse = async (id) => {
 
 const handleForm = async (e) => {
 	e.preventDefault();
+	const course = await getCourse();
 
 	const data = new FormData(form);
+	data.append(
+		'booked_courses',
+		`(${course.title} ${course.date} - ${course.location}) / `
+	);
 	const body = Object.fromEntries(data);
-	console.log(body);
-	try {
-		const response = await fetch('http://localhost:3000/clients', {
-			method: 'POST',
-			body: JSON.stringify(body),
-		});
-		console.log(await response.json());
-		form.reset();
-	} catch (error) {
-		console.error(error);
+	//console.log(body);
+	const clientExist = await checkClient(body.email);
+	if (clientExist === '') {
+		try {
+			const response = await fetch('http://localhost:3000/clients', {
+				method: 'POST',
+				headers: { 'Content-Type': 'Application/json' },
+				body: JSON.stringify(body),
+			});
+			//console.log(await response.json());
+			form.reset();
+		} catch (error) {
+			console.error(error);
+		}
+	} else {
+		const client = await fetch(
+			`http://localhost:3000/clients/${clientExist}`
+		);
+		//	const test = fetch(`http://localhost:3000/clients/${clientExist}`).then(
+		//		(res) => res.json());
+
+		const bookedCourses = await client.json();
+		const response = await fetch(
+			`http://localhost:3000/clients/${clientExist}`,
+			{
+				method: 'PATCH',
+				headers: { 'Content-Type': 'Application/json' },
+				body: JSON.stringify({
+					booked_courses: `${bookedCourses.booked_courses} (${course.title} ${course.date} - ${course.location})`,
+				}),
+			}
+		);
 	}
+};
+
+const checkClient = async (email) => {
+	const response = await fetch('http://localhost:3000/clients');
+	let id;
+	if (response.ok) {
+		data = await response.json();
+
+		for (const key in data) {
+			if (data[key].email === email) {
+				//console.log(data[key].email);
+				console.log('Finns redan denna mail.');
+				id = data[key].id;
+				//break;
+			} else {
+				id = '';
+			}
+		}
+		return id;
+	}
+};
+
+const getCourse = async () => {
+	const id = location.search.split('=')[1];
+	const response = await fetch(`http://localhost:3000/courses/${id}`);
+	const data = await response.json();
+	//console.log(data);
+	return data;
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
